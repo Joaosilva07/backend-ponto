83720c86c7bb5c7c28a50433154f4ba7d77349a5
Joaosilva07 <joao.silva07@sempreceub.com>
2024-11-05 08:27:42 -0300

    Test

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require
+const app = express()
+const port = 3000
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send("deu certo")
+})
+
+// reotrnar algo
+
+app.get('/teste', (req, res) => {
+    res.send("deu certo, retornei algo")
+})
+
+app.get('/teste/:id', (req, res) => {
+    res.send("deu certo, retornei algo com id")
+})
+
+app.post('/testepost', (req, res) => {
+    res.send("deu certo, retornei algo com post")
+})
